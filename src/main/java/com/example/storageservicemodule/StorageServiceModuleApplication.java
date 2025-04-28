373c9d93adb24568c8a50db0548356f7627db71e
Hineill <hineill.cespedes@pucp.edu.pe>
2025-04-28 14:35:26 -0500

    first commit

---
+++ src/main/java/com/example/storageservicemodule/StorageServiceModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.storageservicemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageServiceModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageServiceModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/storageservicemodule/StorageServiceModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storageservicemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
